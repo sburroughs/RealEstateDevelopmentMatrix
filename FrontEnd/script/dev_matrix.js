0ffaef18a86b547f60492d90847ac3041264f333
sburroughs <shane.burroughs@proquest.com>
2016-02-12 09:11:17 -0600

    Removed java project files, site is completely static

--- FrontEnd/script/dev_matrix.js
+++ FrontEnd/script/dev_matrix.js
@@ -4,7 +4,27 @@ var MAX_TASKS = 8;
 var currentStage = 1;
 var currentTask = 1;
 
-var matrix;
+var stages = [
+    "Land Banking",
+    "Land Packaging",
+    "Land Development",
+    "Building Development",
+    "Building Operations",
+    "Building Renovation",
+    "Property Redevelopment",
+];
+
+
+var tasks = [
+    "Acquisition",
+    "Financing",
+    "Market Studies and Marketing Strategies",
+    "Environmental",
+    "Approvals and Permits",
+    "Physical Improvements",
+    "Transportation and Accessibility",
+    "Sales and Disposition"
+];
 
 function navigateContent(stageIncrement, taskIncrement) {
 
@@ -21,10 +41,10 @@ function serveContent(stage, task) {
     currentStage = stage;
     currentTask = task;
 
-    //$('#main-view').empty();
-
     var contentPath = "pages/matrix/stage" + stage + "/task" + task + ".html";
-    $('#main-view').load(contentPath);
+    $('#modal_view').load(contentPath);
+
+    loadComments(currentStage, currentTask);
 }
 
 function validate(stageIndex, taskIndex) {
@@ -48,4 +68,37 @@ function validate(stageIndex, taskIndex) {
     }
 
     return valid;
+}
+
+function loadComments(stageIndex, taskIndex) {
+
+
+    var url = "https://groups.google.com/forum/embed/?place=forum/real-estate-development-matrix" +
+        "&showsearch=true&showpopout=true&" +
+        "parenturl=" + encodeURIComponent(window.location.href) +
+        '#!categories/real-estate-development-matrix/';
+
+    if (stageIndex <= 0) {
+        var taskName = encodeGoogleGroupsSyntax(tasks[taskIndex - 1]);
+
+        url += taskName;
+    }
+    else if (taskIndex <= 0) {
+        var stageName = encodeGoogleGroupsSyntax(stages[stageIndex - 1]);
+
+        url += stageName;
+    }
+    else {
+        var stageName = encodeGoogleGroupsSyntax(stages[stageIndex - 1]);
+        var taskName = encodeGoogleGroupsSyntax(tasks[taskIndex - 1]);
+
+        url += stageName + '/' + taskName;
+    }
+
+    document.getElementById("comments").src = url;
+
+}
+
+function encodeGoogleGroupsSyntax(name) {
+    return name.toLowerCase().split(" ").join("-");
 }
