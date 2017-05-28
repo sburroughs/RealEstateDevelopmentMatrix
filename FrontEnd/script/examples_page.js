59055a59d709cf2657db1f4477c755292e251e4f
Shane Burroughs <Shane.Burroughs@gmail.com>
2017-05-27 19:51:00 -0500

    updated examples page to improve filter ability.

--- FrontEnd/script/examples_page.js
+++ FrontEnd/script/examples_page.js
@@ -7,23 +7,39 @@ function filter() {
     var stages = $('#stageSelect').val();
 
     var allTaskSelector = "#tasks > div[id^='task']";
+    var allStageSelector = allTaskSelector + "> div > div[id*='stage']";
     var taskElements = $(allTaskSelector);
+    var stageElements = $(allStageSelector);
 
     //Unhides All Cards
     taskElements.show();
+    stageElements.show();
 
+    //Hides unselected stages
+    stageElements.each(function () {
+        // removes stage + 1 single digit.
+        // Will not support muli digit stages or tasks
+        var match = this.id.slice(7, 8);
+        if (!stages.includes(match)) {
+            $(this).hide();
+        }
+    });
 
+    //Hides unselected tasks
     var taskSelector = "";
     tasks.forEach(function (taskNumber) {
         taskSelector += "#task" + taskNumber + ",";
     });
     taskSelector = taskSelector.slice(0, -1); //removes trailing ,
-
-    //Hides unselected tasks
     taskElements.not(taskSelector).hide();
 
-    //Hides unselected stages
+    //Filters by text input
+    var searchText = $('#searchText').val().toUpperCase();
+    var searchTextPath = "#tasks > div > div > div > div > h4";
 
-
+    $(searchTextPath).parent().show().filter(function () {
+        var title = $(this).find("h4").text().toUpperCase();
+        var includes = title.includes(searchText);
+        return !includes;
+    }).hide();
 }
-
